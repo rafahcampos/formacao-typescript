b639c07189c0027a38b86bfb2812cb0f4e2b8101
Rafael <rafael.campos.m@hotmail.com>
2025-06-26 10:24:07 -0300

    Segundo desafio

--- Desafio 1 - Desenvolvendo o Dio Bank/src/index.ts
+++ Desafio 1 - Desenvolvendo o Dio Bank/src/index.ts
@@ -3,26 +3,42 @@ import { PeopleAccount } from "./models/PeopleAccount";
 import { SalaryAccount } from "./models/SalaryAccount";
 
 const peopleAccount: PeopleAccount = new PeopleAccount("Roger", 22, 235);
-const companyAccount:CompanyAccount = new CompanyAccount("DIO", 34)
+const peopleAccount2: PeopleAccount = new PeopleAccount("Rogerio", 2222, 235235);
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 34)
+const companyAccount2: CompanyAccount = new CompanyAccount("DIO DIO", 88)
 
 peopleAccount.deposit(1000);
-peopleAccount.withdraw(1700);
+peopleAccount.withdraw(700);
+peopleAccount2.deposit(2000);
+peopleAccount2.withdraw(400);
+
 
 console.log(peopleAccount);
+console.log(peopleAccount2);
 console.log("---------------------");
 
 companyAccount.deposit(15000);
 companyAccount.getLoan(5000);
+companyAccount2.deposit(30000);
+companyAccount2.getLoan(10000);
+
 
 console.log(companyAccount);
+console.log(companyAccount2);
 console.log("---------------------");
 
 const salaryAccount = new SalaryAccount("João", 122);
+const salaryAccount2 = new SalaryAccount("JoãoZinho", 122221);
 
 salaryAccount.depositSalary(100);
 salaryAccount.depositSalary(200);
+salaryAccount2.depositSalary(1000);
+salaryAccount2.depositSalary(2000);
+
+
 
 console.log(salaryAccount);
+console.log(salaryAccount2);
 console.log("---------------------");
 
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-### Formação TypeScript
+## Formação TypeScript
 
-** Desafios desenvolvidos baseados nos cursos da plataforma DIO **
+ * Estes projetos foram desenvolvidos com TypeScript e React, utilizando diversas bibliotecas, como parte de desafios propostos em cursos da plataforma DIO. Todos foram reunidos em um único repositório por praticidade, com foco em aprendizado.
+
+ (https://www.dio.me/)
+
